cb745216749d06f0216d042096e89e0ba466d078
Temirlan <telzhanov94@gmail.com>
2022-08-25 00:59:42 +0600

    Замена на противоположные

---
+++ OppositeElements/Program.cs
@@ -0,0 +1,26 @@
+﻿void PrintArray(int[] array) 
+{ 
+foreach(var element in array) 
+Console.Write($"{element} "); 
+} 
+int[] CreateArray(int size, int minimum, int maximum) 
+{ 
+int[] array= new int[size]; 
+var random = new Random(); 
+for(int i=0;i<size;i++) 
+array[i] = random.Next(minimum,maximum+1); 
+return array; 
+} 
+void Opposit(int [] array)
+{
+    for (int i=0;i<array.Length;i++)
+    array[i]=-array[i];
+} 
+Console.Write("Enter size of array: " ); 
+int size = int.Parse(Console.ReadLine() ?? "0"); 
+int[] array = CreateArray(size,-90,90); 
+Console.Write("Your array: "); 
+PrintArray(array); 
+Console.WriteLine(); 
+Opposit(array);
+PrintArray(array);
