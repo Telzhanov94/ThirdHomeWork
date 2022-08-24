54aa6e4d63de15494dcad6177a70bb6ae09d69e1
Temirlan <telzhanov94@gmail.com>
2022-08-24 23:35:26 +0600

    Thanks, Oleg)))

---
+++ Array8Elements/Program.cs
@@ -0,0 +1,18 @@
+﻿void PrintArray(int[] array) 
+{ 
+foreach(var element in array) 
+Console.Write($"{element} "); 
+} 
+int[] CreateArray(int size, int minimum, int maximum) 
+{ 
+int[] array= new int[size]; 
+var random = new Random(); 
+for(int i=0;i<size;i++) 
+array[i] = random.Next(minimum,maximum+1); 
+return array; 
+} 
+Console.Write("Enter size of array: " ); 
+int size = int.Parse(Console.ReadLine() ?? "0"); 
+int[] array = CreateArray(size,0,1); 
+Console.Write("Your array: "); 
+PrintArray(array); 
